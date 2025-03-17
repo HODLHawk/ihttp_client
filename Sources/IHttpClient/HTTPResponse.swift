c5c7e5af7abc48a0d2d3fc5295dd7c5c27228446
stepan-bezhuk-axon <sbezhuk@axon.dev>
2025-03-17 10:13:36 +0200

    perf: adaptive library for other projects

--- Sources/IHttpClient/HTTPResponse.swift
+++ Sources/IHttpClient/HTTPResponse.swift
@@ -7,7 +7,12 @@
 
 import Foundation
 
-public struct HTTPResponse<T> {
+public struct HTTPResponse<T: Decodable & Sendable>: Sendable {
   let data: T
   let response: URLResponse
+  
+  public init(data: T, response: URLResponse) {
+    self.data = data
+    self.response = response
+  }
 }
