8a641e53cfe050a17dcaa9b552bae263f899fc3d
stepan-bezhuk-axon <sbezhuk@axon.dev>
2025-04-04 12:13:23 +0300

    feat: added extension for http error model

--- Sources/IHttpClient/HTTPError.swift
+++ Sources/IHttpClient/HTTPError.swift
@@ -7,9 +7,34 @@
 import Foundation
 
 let unknowClientError: String = "Unknown client error"
+let clientErrorOccurred: String = "Client error occurred"
+let serverErrorOccurred: String = "Server error occurred"
+let statusCodeStr: String = "Status Code"
 
 public enum HTTPError: Error, Sendable {
   case unknown
   case clientError(Int, APIErrorResponse?)
   case serverError(Int)
+}
+
+extension HTTPError {
+  var message: String {
+    switch self {
+    case .unknown:
+      return unknowClientError
+    case .clientError(_, let apiErrorResponse):
+      return apiErrorResponse?.message ?? clientErrorOccurred
+    case .serverError(let statusCode):
+      return "\(serverErrorOccurred), \(statusCodeStr) : \(statusCode)"
+    }
+  }
+  
+  var code: String? {
+    switch self {
+    case .clientError(_, let apiErrorResponse):
+      return apiErrorResponse?.code
+    default:
+      return nil
+    }
+  }
 }
